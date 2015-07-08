d9849561bbdbf9605699c113d5334051d4375c84
Carlos Fagiani Junior <fagianijunior@gmail.com>
2015-07-08 20:51:44 +0000

    add autenticacao

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+r1 = Role.create({name: "Regular", description: "Can read posts"})
+r2 = Role.create({name: "Writer", description: "Can read and create posts. Can update and destroy own posts"})
+r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
+ 
+u1 = User.create({name: "Carlos Fagiani Junior", email: "fagianijunior@gmail.com", password: "1qaz2wsx", password_confirmation: "1qaz2wsx", role_id: r1.id})
+u2 = User.create({name: "Aletéia Fagiani", email: "ale@g.com", password: "1qaz2wsx", password_confirmation: "1qaz2wsx", role_id: r2.id})
+u3 = User.create({name: "Letícia Fagiani", email: "leh@g.com", password: "1qaz2wsx", password_confirmation: "1qaz2wsx", role_id: r3.id})
