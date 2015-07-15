b9aeb3046275c02724d2594eb83c6343af8ddbd4
Carlos Fagiani Junior <fagianijunior@gmail.com>
2015-07-15 14:23:34 +0000

    navbar com helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  #Helper para altomatizar uma navbar
+  def nav_bar
+    content_tag(:ul, class: "nav navbar-nav") do
+      yield
+    end
+  end
+
+  #cria os links da navbar dentro da tag li
+  def nav_link(text, path)
+    #seta como ativo o link para o controller ativo
+    options = current_page?(path) ? { class: "active" } : {}
+    content_tag(:li, options) do
+      link_to text, path
+    end
+  end
 end
