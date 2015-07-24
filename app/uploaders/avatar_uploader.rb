b2a11ea25aba773f84a94d35aefa2ab6e21413f1
Carlos Fagiani Junior <fagianijunior@gmail.com>
2015-07-24 14:26:43 +0000

    upload organizado de imagens e cloudinary developer

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,20 @@
 # encoding: utf-8
+
 class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   process :convert => 'png'
   process :tags => 'user_avatar'
+
+  def public_id
+    "uploads/#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
+  end
+
+  def filename
+    "avatar.png" if original_filename
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
